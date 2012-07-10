4092daa904eb576363bfc35fd90cb7cd023ff117
MichalBorysiak <borysiam@feynman.(none)>
2012-07-10 14:42:06 +0200

    Fixes in sorting.

--- app/mturk/main/forms.py
+++ app/mturk/main/forms.py
@@ -5,26 +5,28 @@ from django import forms
 from haystack.forms import SearchForm
 
 
-class HitGroupContentSearchForm(SearchForm):
-
-    ORDERS = ("asc", "desc")
-    ORDERS_STRINGS = ("ascending", "descending")
-
-    FIELDS = ("title", "description", "content", "keywords", "requester_name",
-              "reward", "qualifications", "occurence_date", "time_alloted")
-    FIELDS_STRINGS = map(lambda s: "{}{}".format(s[0].upper(), s[1:]), FIELDS)
-    FIELDS_STRINGS = map(
+FIELDS = ("title", "description", "content", "requester_name", "reward",
+          "occurrence_date", "time_alloted", "keywords","qualifications")
+FIELDS_PRETTY = tuple(
+        map(
             lambda s: " ".join(map(string.capitalize, s.split("_"))),
             FIELDS
-        )
+        ))
+FIELDS_PRETTY_DICT = dict(zip(FIELDS, FIELDS_PRETTY))
+ORDERS = ("asc", "desc")
+ORDERS_STRINGS = ("ascending", "descending")
+SORT_BY_FIELDS = tuple(FIELDS[:7])
+PAGE_SIZES = ("5", "10", "20", "50")
+SEARCH_IN_CHOICES = zip(FIELDS, FIELDS_PRETTY)
+SORT_BY_CHOICES = zip(map(lambda order: "{}_{}".format(order[0], order[1]),
+                          product(ORDERS, SORT_BY_FIELDS)),
+                      map(lambda order: "{} ({})".format(
+                                FIELDS_PRETTY_DICT[order[1]],
+                                order[0]),
+                          product(ORDERS_STRINGS, SORT_BY_FIELDS)))
+HITS_PER_PAGE_CHOICES = zip(PAGE_SIZES, PAGE_SIZES)
 
-    SEARCH_IN_CHOICES = zip(FIELDS, FIELDS_STRINGS)
-    SORT_BY_CHOICES = zip(map(lambda order: "{}_{}".format(order[0], order[1]),
-                              product(ORDERS, FIELDS)),
-                          map(lambda order: "{} ({})".format(order[1], order[0]),
-                              product(ORDERS_STRINGS, FIELDS_STRINGS)))
-    PAGE_SIZES = ("5", "10", "20", "50")
-    HITS_PER_PAGE_CHOICES = zip(PAGE_SIZES, PAGE_SIZES)
+class HitGroupContentSearchForm(SearchForm):
 
     search_in = forms.MultipleChoiceField(
             choices=SEARCH_IN_CHOICES,
@@ -52,9 +54,10 @@ class HitGroupContentSearchForm(SearchForm):
         query = cleaned_data.get("q", "")
         if not query:
             return search_queryset
-        search_in = cleaned_data.get("search_in", self.FIELDS)
+        search_in = cleaned_data.get("search_in", FIELDS)
         sort_by = cleaned_data.get("sort_by", "asc_title").split("_")
-        sort_by = "{}{}".format("" if sort_by[0] == "asc" else "-", sort_by[1])
+        sort_by = "{}{}".format("" if sort_by[0] == "asc" else "-",
+                                "_".join(sort_by[1:]))
         kwargs = {}
         for field in search_in:
             kwargs[field] = query
@@ -72,7 +75,3 @@ class HitGroupContentSearchForm(SearchForm):
         hits_per_page = "".join(map(lambda hpp: "&hits_per_page={}".format(hpp),
                                 [cleaned_data.get("hits_per_page", "5")]))
         return "?q={}{}{}{}".format(query, search_in, sort_by, hits_per_page)
-
-    def search_in_selected(self):
-        return self.cleaned_data_or_empty().get("search_in", [])
-
