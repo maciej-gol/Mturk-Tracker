c7240d7986bd72ac6c3039ce77204a488f99de23
Grzegorz Bagrowski <gbagrowski@gmail.com>
2012-07-15 16:51:08 +0200

    Adding a wrapper for arrivals commands.
    Taking the same time arguments as other arrivals commands:
    db_hits_temp_population, db_hits_update and db_reward_population new
    command (db_arrivals) will call all the three mentioned in the correct
    order.
    Furthermore it will dividide the crawls in groups of 10, overlapped by
    one element reporting progress as it's done.
    
    Additionally, db_procedure_command was updated to honour verbosity
    argument and updated a bit.

--- app/mturk/main/management/commands/db_procedure_command.py
+++ app/mturk/main/management/commands/db_procedure_command.py
@@ -7,9 +7,7 @@ from optparse import make_option
 from django.db import connection
 from django.utils.timezone import now
 from django.core.management.base import BaseCommand
-
 from utils.pid import Pid
-
 
 log = logging.getLogger(__name__)
 
@@ -28,45 +26,61 @@ class DBProcedureCommand(BaseCommand):
             help='Minutes to look back for records.'),
         make_option('--hours', dest="hours", default=None, type='int',
             help='Hours to look back for records.'),
+        make_option('--pidfile', dest="pidfile", default=None,
+            help='The pidfile to use.'),
         )
+
+    def process_args(self, options):
+
+        if (options.get('minutes') is not None or
+            options.get('hours') is not None):
+
+            timedelta = datetime.timedelta(
+                minutes=options.get('minutes') or 0,
+                hours=options.get('hours') or 0)
+        else:
+            timedelta = datetime.timedelta(hours=2)
+
+        if options['end'] and options['start'] is None:
+            start = options['end'] - timedelta
+            end = options['end']
+        else:
+            start = options['start'] or (now() - timedelta)
+            end = options['end'] or now()
+
+        self.pidfile = (options.get('pidfile') or
+                        getattr(self, 'pidfile', None) or
+                        self.proc_name)
+        self.start = start
+        self.end = end
+
+        if options.get('verbosity') == 0:
+            self.logger.setLevel(logging.WARNING)
+
+        return start, end
 
     def handle(self, **options):
 
-        pid = Pid(self.proc_name, True)
+        self.process_args(options)
 
+        pid = Pid(self.pidfile, True)
         start_time = time.time()
 
         try:
 
-            if (options.get('minutes') is not None or
-                options.get('hours') is not None):
-
-                timedelta = datetime.timedelta(
-                    minutes=options.get('minutes') or 0,
-                    hours=options.get('hours') or 0)
-            else:
-                timedelta = datetime.timedelta(hours=2)
-
-            if options['end'] and options['start'] is None:
-                start = options['end'] - timedelta
-                end = options['end']
-            else:
-                start = options['start'] or (now() - timedelta)
-                end = options['end'] or now()
-
             cur = connection.cursor()
 
             self.logger.info('Calling {0}({1}, {2}), start time: {3}.'.format(
-                self.proc_name, start, end, now()))
+                self.proc_name, self.start, self.end, now()))
 
-            cur.callproc(self.proc_name, [start, end])
+            cur.callproc(self.proc_name, [self.start, self.end])
 
             cur.execute('commit;')
 
             self.logger.info('{0} for crawls from {1} to {2} took: {3}'.format(
-                self.proc_name, start, end, time.time() - start_time))
+                self.proc_name, self.start, self.end, time.time() - start_time))
 
         except Exception as e:
             log.exception(e)
-
-        pid.remove_pid()
+        finally:
+            pid.remove_pid()
