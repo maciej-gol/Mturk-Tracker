a2c449ef8ac2fc66233337cd204f0046f5192dbe
Grzegorz Bagrowski <gbagrowski@gmail.com>
2012-08-06 15:59:12 +0200

    Updated cache_toprequesters.
    The command was given a complete revamp. A number of useful options
    were added: purge, list. Additionally, reports can be chosen using
    --report-type=<id> or --all options.
    
    After any operation, status is printed, showing which commands are
    available. The command now includes a lot of help texts regarding
    input.

--- app/mturk/toprequesters/management/commands/cache_toprequesters.py
+++ app/mturk/toprequesters/management/commands/cache_toprequesters.py
@@ -1,6 +1,5 @@
 import time
 import logging
-from django.core.cache import cache
 from utils.pid import Pid
 
 from django.core.management.base import BaseCommand, NoArgsCommand
@@ -13,44 +12,90 @@ log = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    """Command for populating toprequesters reports into cache.
+
+    Allows the user to see available reports and their status, cache all or
+    selected reports.
+
+    """
+
     option_list = NoArgsCommand.option_list + (
         make_option('--days', dest='days', default='30',
             help='Number of days from which the history data is grabbed.'),
         make_option('--force', dest='force', action="store_true", default=False,
             help='Enforces overriding existing entry in the cache.'),
+        make_option('--all', dest='all', action="store_true", default=False,
+            help='Evaluates all reports available.'),
+        make_option('--list', dest='list', action="store_true", default=False,
+            help='Shows a list of available report types.'),
+        make_option('--purge', dest='purge', action="store_true", default=False,
+            help='Removes all or the selected report from cache.'),
         make_option('--report-type', dest='report-type', type="int",
-            default=ToprequestersReport.AVAILABLE,
-            help='The report to rebuild.'),
+            default=None, help='The report to rebuild.'),
     )
     help = 'Make sure top requesters are in cache.'
 
     def handle(self, **options):
+        """Main command entry point."""
 
-        pid = Pid('mturk_cache_topreq', True)
+        self.options = options
 
-        report_type = options.get('report-type')
-        if report_type not in ToprequestersReport.values:
-            log.info('Unknown report type: "{0}".'.format(report_type))
-            return
+        if self.options['list']:
+            pass  # do nothing, go straight to print_status
+        else:
+            pid = Pid('mturk_cache_topreq', True)
+            self.prepare_options()  # sets self.reports and prints errors is any
+            self.reports and (self.handle_purge() or self.handle_cache())
+            pid.remove_pid()
 
-        key = ToprequestersReport.get_cache_key(report_type)
+        self.print_status()
+
+    def prepare_options(self):
+        """Sets self.reports with an array of data to process or None.
+        Prints any errors occuring.
+
+        """
+        self.reports = None
+        if self.options.get('all'):
+            self.reports = ToprequestersReport.values
+        else:
+            report_type = self.options.get('report-type')
+            if report_type in ToprequestersReport.values:
+                self.reports = [report_type]
+            else:
+                if report_type is None:
+                    log.info('Please specify "--report-type=<type>" or '
+                        '"--all".')
+                else:
+                    log.info('Unknown report type: "{0}".'.format(
+                        report_type))
+
+    def handle_cache(self):
+        """Handles the primary cache function of this command."""
+        log.info('Caching reports: {0}.'.format(self.reports))
+        for report_type in self.reports:
+            self.__cache_report(report_type)
+
+    def __cache_report(self, report_type):
+        """Evaluates the report and stores it under correct cache key."""
+
         display_name = ToprequestersReport.display_names[report_type]
 
-        if cache.get(key) is None:
-            log.info(('"{0}" toprequesters report missing, recalculating.'
-                ).format(display_name))
-        else:
-            if options['force']:
+        if ToprequestersReport.is_cached(report_type):
+            if self.options['force']:
                 log.info('Recalculating "{0}" toprequesters report.'.format(
                     display_name))
             else:
                 log.info('"{0}" toprequesters still in cache, use --force flag'
                     ' to rebuild anyway.'.format(display_name))
-                return
+                return False
+        else:
+            log.info(('"{0}" toprequesters report missing, recalculating.'
+                ).format(display_name))
 
-        days = options['days']
-        # no chache perform query:
         start_time = time.time()
+
+        days = self.options['days']
         data = ToprequestersReport.REPORT_FUNCTION[report_type](days)
         log.info('Toprequesters report "{0}" generated in: {1}s.'.format(
             display_name, time.time() - start_time))
@@ -59,9 +104,22 @@ class Command(BaseCommand):
         if not data:
             log.warning('Data returned by report function is {0}!'.format(data))
         else:
-            cache.set(key, data, HOURS4)
-            in_cache = cache.get(key, data)
-            if in_cache is None:
+            ToprequestersReport.store(report_type, data)
+            if not ToprequestersReport.is_cached(report_type):
                 log.warning('Cache error - data could not be fetched!')
+        return True
 
-        pid.remove_pid()
+    def handle_purge(self):
+        """Handles purge option. Removes all data if --all was used or spefified
+        --report-type."""
+        if self.options['purge']:
+            log.info("Removing data for {0}.".format(self.reports))
+            for r in self.reports:
+                ToprequestersReport.purge(r)
+            self.print_status()
+        return self.options['purge']
+
+    def print_status(self):
+        """Shows report availability report."""
+        log.info("Available report types: \n" +
+                ToprequestersReport.get_available_as_str())
--- app/mturk/toprequesters/reports.py
+++ app/mturk/toprequesters/reports.py
@@ -141,12 +141,17 @@ class ToprequestersReport:
     #TODO: Add EnumMetaClass to the project and replace repetitive code below
     #TODO2: Test and remove the long-named option
     #TODO3: Add store_report_data method
+    cache_key_base = "TOPREQUESTERS_CACHED_"
 
     AVAILABLE = 0
     POSTED = 1
     POSTED_CRAWL_ID_QUERY_VARIANT = 2
 
-    values = [AVAILABLE, POSTED, POSTED_CRAWL_ID_QUERY_VARIANT]
+    values = [
+        AVAILABLE,
+        POSTED,
+        POSTED_CRAWL_ID_QUERY_VARIANT
+    ]
     display_names = {
         AVAILABLE: 'Hits available',
         POSTED: 'Hits posted',
@@ -160,13 +165,47 @@ class ToprequestersReport:
         POSTED_CRAWL_ID_QUERY_VARIANT: topreq_data_hits_posted_crawl_id,
     }
 
-    @staticmethod
-    def get_cache_key(value):
+    @classmethod
+    def get_cache_key(cls, report_type):
         """Returns string key, under which the report should be cached."""
-        return 'TOPREQUESTERS_CACHED_' + str(value)
+        return cls.cache_key_base + str(report_type)
 
-    @staticmethod
-    def get_report_data(value):
+    @classmethod
+    def is_cached(cls, report_type):
+        """True is there is something under report key in the cache."""
+        return cls.get_report_data(report_type) is not None
+
+    @classmethod
+    def get_report_data(cls, report_type):
         """Returns report data if available or None."""
-        key = ToprequestersReport.get_cache_key(value)
-        return cache.get(key)
+        return cache.get(cls.get_cache_key(report_type))
+
+    @classmethod
+    def get_available_as_str(cls):
+        """Returns a string consising of lines:
+
+            [available] id - Display name
+
+        Example:
+
+            [x] 0 - Hits available
+            [ ] 1 - Hits posted
+
+        """
+        lines = []
+        for rid, name in cls.display_names.iteritems():
+            in_cache = cls.is_cached(rid)
+            in_cache = '[x]' if in_cache else '[ ]'
+            lines.append("\n{0} {1} - {2}".format(in_cache, rid, name))
+        lines.append('\n')
+        return ''.join(lines)
+
+    @classmethod
+    def store(cls, report_type, data, cache_expiry=60 * 60 * 4):
+        """Stores the data under correct cache key."""
+        return cache.set(cls.get_cache_key(report_type), data, cache_expiry)
+
+    @classmethod
+    def purge(cls, report_type):
+        """Deletes all the data related to the given report type from cache."""
+        return cache.delete(cls.get_cache_key(report_type))
