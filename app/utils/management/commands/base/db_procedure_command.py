d9324f1a4bcf2f82e3a2b46e1df7db55d8f311aa
Grzegorz Bagrowski <gbagrowski@gmail.com>
2012-08-28 17:07:39 +0200

    Updating db_procedure_command.
    Procedure will now use TimeArgsCommand as it's base class. Additionally
    introducing a number of minor improvements.

--- app/utils/management/commands/base/db_procedure_command.py
+++ app/utils/management/commands/base/db_procedure_command.py
@@ -1,77 +1,46 @@
 # -*- coding: utf-8 -*-
 import time
-import datetime
 import logging
 
-import dateutil.parser
 from optparse import make_option
-from django.db import connection
+from django.db import connection, transaction
 from django.utils.timezone import now
-from django.core.management.base import BaseCommand
+
+from utils.management.commands.base.time_args_command import TimeArgsCommad
 from utils.pid import Pid
 
 log = logging.getLogger(__name__)
 
 
-class DBProcedureCommand(BaseCommand):
-    help = ''
-    proc_name = ''
-    logger = log
+class DBProcedureCommand(TimeArgsCommad):
 
-    option_list = BaseCommand.option_list + (
-        make_option('--start', dest="start", default=None,
-            help='Processed interval start, defaults to 2 hours.'),
-        make_option('--end', dest="end", default=None,
-            help='Processed interval end, defaults to now.'),
-        make_option('--minutes', dest="minutes", default=None, type='int',
-            help='Minutes to look back for records.'),
-        make_option('--hours', dest="hours", default=None, type='int',
-            help='Hours to look back for records.'),
+    help = 'Django management command for running a stored function.'
+    proc_name = ''
+    """Name of the procedure in the database."""
+    logger = log
+    """Logger instance to use. Altered by command line paremeters."""
+
+    option_list = TimeArgsCommad.option_list + (
         make_option('--pidfile', dest="pidfile", default=None,
             help='The pidfile to use.'),
         make_option('--logger', dest="logger", default=None,
             help='The logger instance to use.'),
         )
 
-    def process_args(self, options):
-
-        if isinstance(options.get('start'), basestring):
-            options['start'] = dateutil.parser.parse(options.get('start'))
-        if isinstance(options.get('end'), basestring):
-            options['end'] = dateutil.parser.parse(options.get('end'))
-
-        if (options.get('minutes') is not None or
-            options.get('hours') is not None):
-
-            timedelta = datetime.timedelta(
-                minutes=options.get('minutes') or 0,
-                hours=options.get('hours') or 0)
-        else:
-            timedelta = datetime.timedelta(hours=2)
-
-        if options['end'] and options['start'] is None:
-            start = options['end'] - timedelta
-            end = options['end']
-        else:
-            start = options['start'] or (now() - timedelta)
-            end = options['end'] or now()
-
-        self.pidfile = (options.get('pidfile') or
+    def process_options(self, options):
+        """Calls parent process_options to populate time relater fields."""
+        self.options = super(DBProcedureCommand, self).process_options(options)
+        self.pidfile = (self.options.get('pidfile') or
                         getattr(self, 'pidfile', None) or
                         self.proc_name)
-        self.start = start
-        self.end = end
-
-        self.logger = logging.getLogger(options.get('logger') or __name__)
-
-        if options.get('verbosity') == 0:
+        self.logger = logging.getLogger(self.options.get('logger') or __name__)
+        if self.options.get('verbosity') == 0:
             self.logger.setLevel(logging.WARNING)
-
-        return start, end
+        return self.options
 
     def handle(self, **options):
 
-        self.process_args(options)
+        self.process_options(options)
 
         pid = Pid(self.pidfile, True)
         start_time = time.time()
@@ -85,9 +54,9 @@ class DBProcedureCommand(BaseCommand):
 
             cur.callproc(self.proc_name, [self.start, self.end])
 
-            cur.execute('commit;')
+            transaction.commit_unless_managed()
 
-            self.logger.info('{0} for crawls from {1} to {2} took: {3}'.format(
+            self.logger.info('{0} for crawls from {1} to {2} took: {3}.'.format(
                 self.proc_name, self.start, self.end, time.time() - start_time))
 
         except Exception as e:
