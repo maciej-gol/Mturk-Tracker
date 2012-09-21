99d0616860849603edeafdfed5c55bddd80db457
Michal Borysiak <borysiam@gmail.com>
2012-09-21 15:15:18 +0200

    MTRACKER-102 small fixes in the classifier path configuration.

--- app/mturk/classification/management/commands/classify.py
+++ app/mturk/classification/management/commands/classify.py
@@ -3,6 +3,8 @@ import logging
 import resource
 
 from optparse import make_option
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.core.management.base import BaseCommand
 
 from mturk.main.models import HitGroupClass
@@ -25,6 +27,14 @@ class ClassifyCommand(BaseCommand):
         make_option('--classifier-path', dest='classifier_path',
                     action='store', default='',
                     help=u'path to the classifier json configuration file'),
+        make_option('--begin', dest='begin',
+                    action='store', default='',
+                    help=u'classify only those groups that were existing '
+                    'in the specified time interval'),
+        make_option('--end', dest='end',
+                    action='store', default='',
+                    help=u'classify only those groups that were existing '
+                    'in the specified time interval'),
     )
 
 
@@ -40,22 +50,44 @@ class ClassifyCommand(BaseCommand):
             logger.info('Removing all existing classification')
             HitGroupClass.objects.all().delete()
             return
+        if options['begin'] and options['end']:
+            # XXX it can be slow.
+            query = ''' SELECT group_id, title, description, keywords
+                        FROM main_hitgroupcontent as content
+                        WHERE NOT EXISTS(
+                            SELECT * FROM main_hitgroupclass as class
+                            WHERE content.group_id = class.group_id
+                        ) LIMIT {};
+                    '''.format(self.BATCH_SIZE)
+        else:
+            query = ''' SELECT group_id, title, description, keywords
+                        FROM main_hitgroupcontent as content
+                        JOIN hits_mv 
+                        ON content.group_id = hits_mv.group_id
+                        WHERE 
+                            NOT EXISTS(
+                                SELECT * FROM main_hitgroupclass as class
+                                WHERE content.group_id = class.group_id
+                            ) AND 
+                            hits_mv.start_time >= {} AND 
+                            hits_mv.start_time < {} 
+                        GROUP BY group_id
+                        LIMIT {};
+                    '''.format(options['begin'], options['end'], self.BATCH_SIZE)
         if not options['classifier_path']:
-            options['classifier_path'] = '~/classifier.json'
+            try:
+                options['classifier_path'] = settings.CLASSIFIER_PATH
+            except AttributeError:
+                raise ImproperlyConfigured('Classifier path is not specified '
+                                           'neither in the settings file'
+                                           'nor in the command line')
         with open(options['classifier_path'], 'r') as file:
             probabilities = json.load(file)
             classifier = NaiveBayesClassifier(probabilities=probabilities)
             logger.info('Classification of hit groups started. Processing in '\
                         'batches size of {}'.format(self.BATCH_SIZE))
             while True:
-                models = query_to_dicts(
-                    ''' SELECT group_id, title, description, keywords
-                        FROM main_hitgroupcontent as content
-                        WHERE NOT EXISTS(
-                            SELECT * FROM main_hitgroupclass as class
-                            WHERE content.group_id = class.group_id
-                        ) LIMIT {};
-                    '''.format(self.BATCH_SIZE))
+                models = query_to_dicts(query)
                 logger.info('Batch classification started')
                 try:
                     results = _to_hit_group_class(classifier.classify_batch(models))
