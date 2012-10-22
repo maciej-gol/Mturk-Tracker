061c4fba8d863fb99c95f19094b0cc75b20c9c03
Grzegorz Bagrowski <gbagrowski@gmail.com>
2012-10-22 12:04:21 +0200

    Average arrivals data report for day/week/month.

--- app/mturk/main/management/commands/sql/reports.sql
+++ app/mturk/main/management/commands/sql/reports.sql
@@ -22,3 +22,26 @@ FROM (
     GROUP BY month
     ORDER BY month DESC
 ) asd;
+
+--
+-- AVERAGE ARRIVALS DATA
+--
+-- Average arrivals data for day, week and month.
+--
+SELECT
+    date_trunc('day', start_time) as unit,
+    count(*) as total,
+    round(CAST (avg(hits_posted) as NUMERIC), 2) as hits_posted,
+    round(CAST (avg(hits_consumed) as NUMERIC), 2) as hits_consumed,
+    round(CAST (avg(rewards_posted) as NUMERIC), 2) as rewards_posted,
+    round(CAST (avg(rewards_consumed) as NUMERIC), 2) as rewards_consumed
+FROM
+    main_crawlagregates
+GROUP BY unit
+ORDER BY unit DESC
+--
+-- The following lines will output .csv files to /tmp/mtracker_reports/.
+--
+echo "COPY (SELECT date_trunc('day', start_time) as unit, count(*) as total, round(CAST (avg(hits_posted) as NUMERIC), 2) as hits_posted, round(CAST (avg(hits_consumed) as NUMERIC), 2) as hits_consumed, round(CAST (avg(rewards_posted) as NUMERIC), 2) as rewards_posted, round(CAST (avg(rewards_consumed) as NUMERIC), 2) as rewards_consumed FROM main_crawlagregates GROUP BY unit ORDER BY unit DESC) TO STDOUT WITH CSV HEADER" | psql -o '/tmp/mtracker_reports/day.csv' -d mturk_tracker_db -U mturk_tracker
+echo "COPY (SELECT date_trunc('week', start_time) as unit, count(*) as total, round(CAST (avg(hits_posted) as NUMERIC), 2) as hits_posted, round(CAST (avg(hits_consumed) as NUMERIC), 2) as hits_consumed, round(CAST (avg(rewards_posted) as NUMERIC), 2) as rewards_posted, round(CAST (avg(rewards_consumed) as NUMERIC), 2) as rewards_consumed FROM main_crawlagregates GROUP BY unit ORDER BY unit DESC) TO STDOUT WITH CSV HEADER" | psql -o '/tmp/mtracker_reports/week.csv' -d mturk_tracker_db -U mturk_tracker
+echo "COPY (SELECT date_trunc('month', start_time) as unit, count(*) as total, round(CAST (avg(hits_posted) as NUMERIC), 2) as hits_posted, round(CAST (avg(hits_consumed) as NUMERIC), 2) as hits_consumed, round(CAST (avg(rewards_posted) as NUMERIC), 2) as rewards_posted, round(CAST (avg(rewards_consumed) as NUMERIC), 2) as rewards_consumed FROM main_crawlagregates GROUP BY unit ORDER BY unit DESC) TO STDOUT WITH CSV HEADER" | psql -o '/tmp/mtracker_reports/month.csv' -d mturk_tracker_db -U mturk_tracker
